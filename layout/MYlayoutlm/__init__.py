e4f692cbfee9861a333f851883ca37a83307bb79
Your Name <you@example.com>
2020-10-16 08:46:00 -0700

    half precision training

---
+++ layout/MYlayoutlm/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+from .data.funsd import FunsdDataset
+from .data.funsd_img import IMFunsdDataset
+from .modeling.layoutlm import (
+    LayoutlmConfig,
+    LayoutlmForSequenceClassification,
+    LayoutlmForTokenClassification,
+)
